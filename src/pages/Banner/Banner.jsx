684267ec2790de2edfb73c7341cb1c8a02b6bff3
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-08-27 13:56:30 +0600

    updated resume

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -31,7 +31,7 @@ const Banner = () => {
                         repeat={Infinity}
                     />
                     <div className="mt-5">
-                        <Link to="https://drive.google.com/file/d/1eS2BXeZfj2g7_kdrdXIW5np7VXx1jfLJ/view?usp=sharing">
+                        <Link to="https://drive.google.com/file/d/1kOWHTdjqUZHx2swd1pi6zbbFY50CGvT2/view?usp=sharing">
                             <button className="btn text-base border-none text-white bg-gradient-to-r from-[#1b495c] via-[#046c92] to-[#1ca4df]  hover:from-[#173a47] hover:via-[#217897] hover:to-[#3e7f9b] ">Download My Resume
                                 <LuDownload className="text-2xl text-white"></LuDownload>
                             </button>
